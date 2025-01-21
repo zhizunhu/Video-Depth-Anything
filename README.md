0c0f21c2dd8dc37288e10c08e92cd4c30c51354f
Shengnan-Zhu <zhu_shengnan@qq.com>
2025-01-21 21:17:27 +0800

    add demo

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Video-Depth-Anything
+<div align="center">
+<h1>Video Depth Anything</h1>
+
+
+</div>
+
+This work presents **Video Depth Anything** based on [Depth Anything V2](https://github.com/DepthAnything/Depth-Anything-V2), which can be applied to arbitrarily long videos without compromising quality, consistency, or generalization ability. Compared with other diffusion-based models, it enjoys faster inference speed, fewer parameters, and higher consistent depth accuracy.
+
+![teaser](assets/teaser_video_v2.png)
+
+## News
+- **2025-01-25:** Paper, project page, code, models, and demo are all released.
+
+
+## Pre-trained Models
+We provide **two models** of varying scales for robust and consistent video depth estimation:
+
+| Model | Params | Checkpoint |
+|:-|-:|:-:|
+| Depth-Anything-V2-Small | 28.4M | Coming soon |
+| Depth-Anything-V2-Large | 381.8M | Coming soon |
+
+
+## Usage
+
+### Prepraration
+
+```bash
+git clone https://github.com/DepthAnything/Video-Depth-Anything
+cd Video-Depth-Anything
+pip install -r requirements.txt
+```
+
+Download the checkpoints listed [here](#pre-trained-models) and put them under the `checkpoints` directory.
+
+### Inference a video
+```bash
+python3 run.py --input_video ./assets/example_videos/basketball.mp4 --output_dir ./outputs --encoder vitl
+```
+
+## Acknowledgement
+
+## LICENSE
+
+## Citation
+
+If you find this project useful, please consider citing:
+
+```bibtex
+```
+
+
+## LICENSE
+Video-Depth-Anything-Small model is under the Apache-2.0 license. Video-Depth-Anything-Large model is under the CC-BY-NC-4.0 license.
---
+++ utils/dc_utils.py
@@ -0,0 +1,79 @@
+# This file may have been modified by Bytedance Ltd. and/or its affiliates (“Bytedance's Modifications”). 
+# All Bytedance's Modifications are Copyright (2025) Bytedance Ltd. and/or its affiliates. 
+from typing import Union, List
+import tempfile
+import numpy as np
+import PIL.Image
+import matplotlib.cm as cm
+import mediapy
+import torch
+from decord import VideoReader, cpu
+
+
+def read_video_frames(video_path, process_length, target_fps=-1, max_res=-1, dataset="open"):
+
+    vid = VideoReader(video_path, ctx=cpu(0))
+    original_height, original_width = vid.get_batch([0]).shape[1:3]
+    height = original_height
+    width = original_width
+    if max_res > 0 and max(height, width) > max_res:
+        scale = max_res / max(original_height, original_width)
+        height = round(original_height * scale)
+        width = round(original_width * scale)
+
+    vid = VideoReader(video_path, ctx=cpu(0), width=width, height=height)
+
+    fps = vid.get_avg_fps() if target_fps == -1 else target_fps
+    stride = round(vid.get_avg_fps() / fps)
+    stride = max(stride, 1)
+    frames_idx = list(range(0, len(vid), stride))
+    if process_length != -1 and process_length < len(frames_idx):
+        frames_idx = frames_idx[:process_length]
+    frames = vid.get_batch(frames_idx).asnumpy()
+
+    return frames, fps
+
+
+def save_video(
+    video_frames: Union[List[np.ndarray], List[PIL.Image.Image]],
+    output_video_path: str = None,
+    fps: int = 10,
+    crf: int = 18,
+) -> str:
+    if output_video_path is None:
+        output_video_path = tempfile.NamedTemporaryFile(suffix=".mp4").name
+
+    if isinstance(video_frames[0], np.ndarray):
+        video_frames = [frame.astype(np.uint8) for frame in video_frames]
+
+    elif isinstance(video_frames[0], PIL.Image.Image):
+        video_frames = [np.array(frame) for frame in video_frames]
+    mediapy.write_video(output_video_path, video_frames, fps=fps, crf=crf)
+    return output_video_path
+
+
+class ColorMapper:
+    # a color mapper to map depth values to a certain colormap
+    def __init__(self, colormap: str = "inferno"):
+        self.colormap = torch.tensor(cm.get_cmap(colormap).colors)
+
+    def apply(self, image: torch.Tensor, v_min=None, v_max=None):
+        # assert len(image.shape) == 2
+        if v_min is None:
+            v_min = image.min()
+        if v_max is None:
+            v_max = image.max()
+        image = (image - v_min) / (v_max - v_min)
+        image = (image * 255).long()
+        image = self.colormap[image] * 255
+        return image
+
+
+def vis_sequence_depth(depths: np.ndarray, v_min=None, v_max=None):
+    visualizer = ColorMapper()
+    if v_min is None:
+        v_min = depths.min()
+    if v_max is None:
+        v_max = depths.max()
+    res = visualizer.apply(torch.tensor(depths), v_min=v_min, v_max=v_max).numpy()
+    return res
